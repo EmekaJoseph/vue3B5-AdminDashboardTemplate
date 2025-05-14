2bd2c405a84a2b9b6be1870cc857e835dc268ecc
emekajoseph <josephiwuji@gmail.com>
2025-05-14 22:35:37 +0100

    added tooltip

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,34 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-// import { useIdleLogout } from '@/stores/inactivityWatcher';
+import { RouterView, useRoute } from 'vue-router';
+import { watch } from 'vue'
 
+// @ts-ignore
+import * as bootstrap from 'bootstrap'
+
+const route = useRoute()
+
+// import { useIdleLogout } from '@/stores/inactivityWatcher';
 // useIdleLogout()
+
+
+watch(() => [route.fullPath, /*templateStore.activateToolTip*/], () => {
+  setTimeout(() => {
+    // Destroy existing tooltips first
+    const existingTooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    existingTooltips.forEach(el => {
+      const tooltip = bootstrap.Tooltip.getInstance(el)
+      if (tooltip) tooltip.dispose()
+    })
+
+    // Re-initialize tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    tooltipTriggerList.forEach(el => {
+      new bootstrap.Tooltip(el)
+    })
+  }, 0)
+})
+
+
 </script>
